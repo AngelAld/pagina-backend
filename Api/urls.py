803bd4ac6311257bc63927a5779fcda22b3382c0
AngelAld <aaaldana50@gmail.com>
2024-11-16 11:45:37 -0500

    media & static urls

--- Api/urls.py
+++ Api/urls.py
@@ -5,6 +5,8 @@ URL configuration for Api project.
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,3 +17,5 @@ urlpatterns = [
     path("", include("Ubicacion.urls")),
     path("", include("Inmuebles.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
